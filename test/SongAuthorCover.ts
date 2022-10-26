11925f296b85ff78668b8ef6db76a14f2994a2fc
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-10-26 19:24:54 -0300

    test: added test cases descriptions

---
+++ test/SongAuthorCover.ts
@@ -0,0 +1,13 @@
+describe("SongAuthorCover", function () {
+  // test case: 
+  // 1. create a SongAuthorCover instance
+  // 2. call safeMint() method passing a valid address and uri
+  // 3. call ownerOf() method passing 0 as token id
+  // 4. returned value should match the address and uri passed to safeMint()
+
+  // test case: 
+  // 1. create a SongAuthorCover instance
+  // 2. call safeMint() method passing a valid address and uri
+  // 3. call transferFrom() method passing a valid address and token id.
+  // 4. should trow an error
+});
