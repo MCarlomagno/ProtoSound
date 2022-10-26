3ed25c826c6e994bbf62d94333736b1695490ae2
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-10-26 11:10:22 -0300

    test: added test case to ProtoSound contract

--- test/ProtoSound.ts
+++ test/ProtoSound.ts
@@ -1,6 +1,11 @@
 describe("ProtoSound", function () {
   // test case: 
   // 1. create a ProtoSound instance
+  // 2. call create() method
+  // 3. call changeNick() method
+  // 4. check the nick of the user.
+
+  // 1. create a ProtoSound instance
   // 2. call the mintSong() method
   // 3. check SongAudio and SongAuthorCover tokens created
   // 4. check SongAuthorCover tokens minted
