02b0bfe98c7ab47e202134a4849e509195b4b58d
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-10-28 13:29:31 -0300

    feat: setup IPFS for client app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,42 @@
-import './App.css'
+import './App.css';
+import * as IPFS from 'ipfs';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [cid, setCid] = useState<string>('');
+  const [node, setNode] = useState<any>();
+  const [text, setText] = useState<string>();
+
+  useEffect(() => {
+    IPFS.create().then(setNode);
+  }, []);
+
+  const upload = async () => {
+    const result = await node.add(text);
+    setCid(result.cid.toString());
+    console.log('done!');
+  }
+
+  const download = async () => {
+    const stream = node.cat(cid)
+    const decoder = new TextDecoder()
+    let data = ''
+    for await (const chunk of stream) {
+      data += decoder.decode(chunk, { stream: true })
+    }
+    console.log(data)
+  }
+
   return (
     <div className="App">
-      <div>Hello wold</div>
+      <div>Upload a text to ipfs</div>
+      <input type="text" onChange={(e) => setText(e.target.value)}/>
+      <button onClick={upload}>Upload</button>
+
+      {cid && <div>your IPFS url is: https://ipfs.io/ipfs/{cid}</div>}
+
+      <div>Now donwload it</div>
+      <button onClick={download}>donwload</button>
     </div>
   )
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    target: 'es2020'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020'
+    }
+  },
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+  },
 })
