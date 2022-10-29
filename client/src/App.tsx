59270e5a21abedf5b15405260e013b17116e16f2
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-10-28 23:14:37 -0300

    feat: upload any file to IPFS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,39 @@
 import './App.css';
+import { useRef, useState } from 'react';
 import * as IPFS from 'ipfs';
-import { useEffect, useState } from 'react';
+
+// IPFS node should be instantiated a single time for a repo
+let ipfs: IPFS.IPFS | undefined;
+IPFS.create().then(((ipfsInstance: any) => ipfs = ipfsInstance))
 
 function App() {
   const [cid, setCid] = useState<string>('');
-  const [node, setNode] = useState<any>();
-  const [text, setText] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const file = useRef<File>();
 
-  useEffect(() => {
-    IPFS.create().then(setNode);
-  }, []);
 
-  const upload = async () => {
-    const result = await node.add(text);
+  const handleOnChange = (files: FileList | null) => {
+    if (!files) return;
+    console.log(files[0]);
+    file.current = files[0];
+  };
+
+  const submit = async () => {
+    if (!file.current || !ipfs) return;
+    setLoading(true);
+    const result = await ipfs.add(file.current);
+    setLoading(false);
     setCid(result.cid.toString());
     console.log('done!');
-  }
-
-  const download = async () => {
-    const stream = node.cat(cid)
-    const decoder = new TextDecoder()
-    let data = ''
-    for await (const chunk of stream) {
-      data += decoder.decode(chunk, { stream: true })
-    }
-    console.log(data)
-  }
+  };
 
   return (
     <div className="App">
-      <div>Upload a text to ipfs</div>
-      <input type="text" onChange={(e) => setText(e.target.value)}/>
-      <button onClick={upload}>Upload</button>
-
+      <h1>IPFS file Upload</h1>
+      <input type="file" onChange={(e) => handleOnChange(e.target.files)} />
+      <button onClick={submit}>Upload</button>
+      {loading && <div>Loading...</div>}
       {cid && <div>your IPFS url is: https://ipfs.io/ipfs/{cid}</div>}
-
-      <div>Now donwload it</div>
-      <button onClick={download}>donwload</button>
     </div>
   )
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
   define: {
     // By default, Vite doesn't include shims for NodeJS/
     // necessary for segment analytics lib to work
-    global: {},
+    global: {
+    },
   },
 })
