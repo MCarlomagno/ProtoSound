666dd2b213f213f82273930b1d581a413e8a8cfe
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-11-03 10:51:11 -0300

    feat: added feed grid

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
       <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-        <Router>
+        <Router basename='ProtoSound'>
           <AppShell header={<AppHeader></AppHeader>}>
             <Routes>
               <Route path="/" element={<Home />} />
--- client/src/pages/Feed/Feed.tsx
+++ client/src/pages/Feed/Feed.tsx
@@ -1,8 +1,87 @@
-import { Title } from '@mantine/core';
+import { SimpleGrid, Title, Text, Card, Image, Center, Container } from '@mantine/core';
+
+const mockdata = [
+  { 
+    author: 'Luca Prodan',
+    name: 'La Rubia Tarada',
+    audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    image: 'https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' },
+  { 
+    author: 'Rolling Stones',
+    name: 'Satisfaction',
+    audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    image: 'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' },
+  { 
+    author: 'The Beatles',
+    name: 'Yesterday',
+    audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    image: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' },
+  { 
+    author: 'Wos',
+    name: 'Protocolo',
+    audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    image: 'https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' },
+  { 
+    author: 'Briney Spears',
+    name: 'Toxic',
+    audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    image: 'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' },
+  { 
+    author: 'La Renga',
+    name: 'La balada del diablo y la muerte',
+    audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    image: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' },
+  { 
+    author: 'Queen',
+    name: 'I want to break free',
+    audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    image: 'https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' },
+  { 
+    author: 'Charly Garcia',
+    name: 'Los Dinosaurios', 
+    audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    image: 'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' },
+  { 
+    author: 'Radiohead',
+    name: 'Karma Police', 
+    audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3', 
+    image: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' },
+];
 
 function Feed() {
+
+  const items = mockdata.map((item) => (
+    <Card withBorder radius="md" p="md" key={item.name}>
+      <Card.Section>
+        <Image src={item.image} alt={item.name} height={150} />
+      </Card.Section>
+      <Card.Section p="md">
+          <Text size="xs" color="dimmed">
+            {item.author}
+          </Text>
+          <Text size="sm" weight={500}>
+            {item.name}
+          </Text>
+      </Card.Section>
+      <Card.Section p="md">
+        <Center>
+          <audio src={item.audio} controls></audio>
+        </Center>
+      </Card.Section>
+    </Card>
+
+  ));
+
   return (
-    <Title p={'lg'} order={2}>Feed</Title>
+    <Container>
+      <Title p={'lg'} order={2}>Feed</Title>
+      <SimpleGrid
+        cols={3}
+        mt="md" 
+        breakpoints={[{ maxWidth: 'sm', cols: 1 }, { maxWidth: 'md', cols: 2 }]}>
+        {items}
+      </SimpleGrid>
+    </Container>
   )
 }
 
