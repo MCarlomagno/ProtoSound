06eb4dbb8cc28a2939eb99442b800f2a939e3ee0
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-11-07 15:21:17 -0300

    ui: move cta to home

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -74,36 +74,9 @@ function Feed() {
           </Tabs.List>
 
           <Tabs.Panel value="created" pt="xs">
-              <BackgroundImage
-                src="https://images.unsplash.com/photo-1429962714451-bb934ecdc4ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-                radius="lg"
-                p={'lg'}
-                my={'lg'}
-              >
-                <Center p="md">
-                  <Stack mx={'md'}>
-                    <Text color={'white'} size={30} weight={600} >
-                      Create, Mint & Earn
-                    </Text>
-                    <Text color={'white'} size={20} weight={500}>
-                      a song, release your personal cover and a collection of covers to engage with your fans
-                    </Text>
-                  </Stack>
-
-                  <Button mx={'md'} leftIcon={<IconDisc size={14} />}>
-                    Create new release
-                  </Button>
-                </Center>
-              </BackgroundImage>
             <Title my={'xl'} order={3}>
               Your releases
             </Title>
-            <SimpleGrid
-              cols={3}
-              mt="md"
-              breakpoints={[{ maxWidth: 'sm', cols: 1 }, { maxWidth: 'md', cols: 2 }]}>
-              {items}
-            </SimpleGrid>
           </Tabs.Panel>
 
           <Tabs.Panel value="collected" pt="xs">
