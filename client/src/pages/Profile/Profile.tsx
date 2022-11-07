e0d867a16351b6bcbe3f295c36b98fbf28667c6b
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-11-07 16:58:15 -0300

    chore: ui styles

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,5 +1,5 @@
-import { Card, Image, Text, Center, Container, SimpleGrid, Avatar, Alert, Group, ActionIcon, Tabs, Dialog, TextInput, Button, Stack, BackgroundImage, Divider, Title } from '@mantine/core';
-import { IconAlertCircle, IconBucket, IconDisc, IconEdit, IconSeeding } from '@tabler/icons';
+import { Card, Image, Text, Center, Container, SimpleGrid, Avatar, Alert, Group, ActionIcon, Tabs, Dialog, TextInput, Button, Stack, BackgroundImage, Divider, Title, Space } from '@mantine/core';
+import { IconAlertCircle, IconBucket, IconDisc, IconEdit, IconMusic, IconSeeding } from '@tabler/icons';
 import { useState } from 'react';
 import mockdata from '../../mockdata.json';
 
@@ -36,17 +36,20 @@ function Feed() {
   return (
     <Container>
       {user.address ? <>
-        <Group my={'md'}>
-          <Avatar radius={'xl'} src={`https://avatars.dicebear.com/api/identicon/${user.address}.svg`} />
-          <div>
-            <Group>
-              <Text size={'xl'} weight={600}>{user.nick}</Text>
-              <ActionIcon>
-                <IconEdit size={14} onClick={() => setEditOpened((o) => !o)} />
-              </ActionIcon>
-            </Group>
-            <Text size={'sm'} weight={200}>{user.address}</Text>
-          </div>
+        <Group style={{ justifyContent:'space-between'}} my={'md'}>
+          <Group>
+            <Avatar radius={'xl'} src={`https://avatars.dicebear.com/api/identicon/${user.address}.svg`} />
+            <div>
+              <Group>
+                <Text size={'xl'} weight={600}>{user.nick}</Text>
+                <ActionIcon>
+                  <IconEdit size={14} onClick={() => setEditOpened((o) => !o)} />
+                </ActionIcon>
+              </Group>
+              <Text size={'sm'} weight={200}>{user.address}</Text>
+            </div>
+          </Group>
+          <Button leftIcon={<IconMusic />}>Release Song</Button>
         </Group>
 
         <Dialog
