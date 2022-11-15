cb10fd1d578c0cacf0075947832ecc6586484307
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-11-15 18:31:01 -0300

    test: fix test case

--- client/src/components/AppHeader/AppHeader.test.tsx
+++ client/src/components/AppHeader/AppHeader.test.tsx
@@ -3,6 +3,8 @@ import '@testing-library/jest-dom'
 import { BrowserRouter as Router } from "react-router-dom";
 import AppHeader from './AppHeader';
 import { ColorSchemeProvider } from '@mantine/core';
+import * as metamask from '../../hooks/useMetamask' 
+
 
 jest.mock('../../assets/logo.svg', () => 'https://test.com/');
 
@@ -10,6 +12,14 @@ describe('AppHeader', () => {
   test('renders AppHeader component', async () => {
     // must render colorscheme + router
     // for setting up the header
+    jest.spyOn(metamask, 'useMetamask').mockImplementation(() => ({
+      connect: jest.fn(),
+      accounts: [''], 
+      getAccounts: jest.fn().mockReturnValue(new Promise(() => {})),
+      network: null,
+      sendTransaction: jest.fn(),
+      signer: null
+    }));
     render(
       <ColorSchemeProvider colorScheme='light' toggleColorScheme={()=> {}}>
           <Router><AppHeader/></Router>
