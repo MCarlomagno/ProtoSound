5badb2bcb33d5a9453841bc4b64843a82f99e3fd
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-11-17 03:00:36 -0300

    fix: build for client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ProtoSound/',
   build: {
     target: 'es2020',
-    outDir: '../dist'
+    outDir: 'dist'
   },
   optimizeDeps: {
     esbuildOptions: {
