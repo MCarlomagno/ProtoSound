266e33e78fef627cb177555561ee107a878541c9
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-11-17 02:35:27 -0300

    refactor: deleted unused component

--- client/src/components/Upload/Upload.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Upload from './Upload';
-
-describe('Upload', () => {
-  test('renders Upload component', async () => {
-    // component not yet defined
-  })
-});
